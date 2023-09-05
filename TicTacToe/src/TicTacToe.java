ef35ebaf4788aae4459d0a1be07a61cb1fc61b33
Jireh Battung <jirehgbattung@gmail.com>
2023-09-05 19:15:39 +0800

    init and starting methods

---
+++ TicTacToe/src/TicTacToe.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+
+public class TicTacToe implements ActionListener{
+	
+	TicTacToe(){
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void firstTurn() {
+		
+	}
+	
+	public void check() {
+		
+	}
+	
+	public void xWins(int a, int b, int c) {
+		
+	}
+	
+	public void oWins(int a, int b, int c) {
+		
+	}
+	
+}
